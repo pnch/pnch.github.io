c9aa4798db81bb3f781425b52c97f58df227532a
pnch <ponchikvazap@gmail.com>
2018-10-02 21:43:17 +0300

    for ivn

---
+++ js/fieldcheck.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".button").click(function(){
+    
+        var number = $(".field_with_number").text();
+        var address = $(".field_with_address").text();
+  
+        $.ajax({
+            type: "POST",
+            url: "/settings/setmin",
+            data: {min: number, addr: address},
+            dataType: "html",
+            contentType: "application/json;charset=utf-8", 
+            success: function(){
+                alert("success!");
+            },
+            fail: function() {
+                alert( "error" );
+              }
+        });
+    
+    });
+  });
